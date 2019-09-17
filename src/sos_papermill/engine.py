6c5954888c4bf29453f028570650781f2fc5b987
Bo Peng <ben.bog@gmail.com>
2019-09-17 08:59:24 -0500

    Move sos preprocessor to sos-papermill vatlab/sos-notebook#251

--- src/sos_papermill/engine.py
+++ src/sos_papermill/engine.py
@@ -3,24 +3,136 @@
 # Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
 # Distributed under the terms of the 3-clause BSD License.
 
+import re
+from nbformat.v4 import output_from_msg
+
 from papermill.engines import Engine
 from papermill.utils import merge_kwargs, remove_args
 from papermill.log import logger
 from papermill.preprocess import PapermillExecutePreprocessor
 from papermill.translators import papermill_translators, PythonTranslator
 
-from sos_notebook.converter import SoS_ExecutePreprocessor, extract_workflow
+from sos.converter import extract_workflow
 
-class SoSPaperMillPreprocessor(SoS_ExecutePreprocessor, PapermillExecutePreprocessor):
+class SoSPaperMillPreprocessor(PapermillExecutePreprocessor):
+
     def __init__(self, filename, *args, **kwargs):
-        #
-        # Note that SoS_ExecutePreprocessor will take filename, and pass the rest to
-        # PapermillExecutePreprocessor
-        super(SoSPaperMillPreprocessor, self).__init__(filename, *args, **kwargs)
+        super(SoSPaperMillPreprocessor, self).__init__(*args, **kwargs)
+        self._filename = filename
+
+    def _prepare_meta(self, cell):
+        meta = {
+            'use_panel': False,
+            'cell_id': '0',
+            'path': self._filename,
+            'batch_mode': True,
+            'cell_kernel': cell.metadata.kernel if hasattr(cell.metadata, 'kernel') else 'SoS'
+            }
+        if re.search(
+            r'^%sosrun($|\s)|^%sossave($|\s)|^%preview\s.*(-w|--workflow).*$',
+            cell.source, re.MULTILINE):
+            meta['workflow'] = self._workflow
+        return meta
+
+    def run_cell(self, cell, cell_index=0, store_history=True):
+        # sos is the additional meta information sent to kernel
+        content = dict(
+            code=cell.source,
+            silent=False,
+            store_history=store_history,
+            user_expressions='',
+            allow_stdin=False,
+            stop_on_error=False,
+            sos=self._prepare_meta(cell))
+        msg = self.kc.session.msg('execute_request', content)
+        self.kc.shell_channel.send(msg)
+        msg_id = msg['header']['msg_id']
+
+        # the reset is copied from https://github.com/jupyter/nbconvert/blob/master/nbconvert/preprocessors/execute.py
+        # because we only need to change the first line
+
+        #  msg_id = self.kc.execute(cell.source)
+
+        self.log.debug("Executing cell:\n%s", cell.source)
+        exec_reply = self._wait_for_reply(msg_id, cell)
+
+        outs = cell.outputs = []
+
+        while True:
+            try:
+                # We've already waited for execute_reply, so all output
+                # should already be waiting. However, on slow networks, like
+                # in certain CI systems, waiting < 1 second might miss messages.
+                # So long as the kernel sends a status:idle message when it
+                # finishes, we won't actually have to wait this long, anyway.
+                msg = self.kc.iopub_channel.get_msg(timeout=self.iopub_timeout)
+            except Empty:
+                self.log.warning("Timeout waiting for IOPub output")
+                if self.raise_on_iopub_timeout:
+                    raise RuntimeError("Timeout waiting for IOPub output")
+                else:
+                    break
+            if msg['parent_header'].get('msg_id') != msg_id:
+                # not an output from our execution
+                continue
+
+            msg_type = msg['msg_type']
+            self.log.debug("output: %s", msg_type)
+            content = msg['content']
+
+            # set the prompt number for the input and the output
+            if 'execution_count' in content:
+                cell['execution_count'] = content['execution_count']
+
+            if msg_type == 'status':
+                if content['execution_state'] == 'idle':
+                    break
+                else:
+                    continue
+            elif msg_type == 'execute_input':
+                continue
+            elif msg_type == 'clear_output':
+                outs[:] = []
+                # clear display_id mapping for this cell
+                for display_id, cell_map in self._display_id_map.items():
+                    if cell_index in cell_map:
+                        cell_map[cell_index] = []
+                continue
+            elif msg_type.startswith('comm'):
+                continue
+
+            display_id = None
+            if msg_type in {
+                    'execute_result', 'display_data', 'update_display_data'
+            }:
+                display_id = msg['content'].get('transient',
+                                                {}).get('display_id', None)
+                if display_id:
+                    self._update_display_id(display_id, msg)
+                if msg_type == 'update_display_data':
+                    # update_display_data doesn't get recorded
+                    continue
+
+            try:
+                out = output_from_msg(msg)
+            except ValueError:
+                self.log.error("unhandled iopub msg: " + msg_type)
+                continue
+            if display_id:
+                # record output index in:
+                #   _display_id_map[display_id][cell_idx]
+                cell_map = self._display_id_map.setdefault(display_id, {})
+                output_idx_list = cell_map.setdefault(cell_index, [])
+                output_idx_list.append(len(outs))
+
+            outs.append(out)
+
+        return exec_reply, outs
 
     def preprocess(self, nbman, *args, **kwargs):
         self._workflow = extract_workflow(nbman.nb)
-        return PapermillExecutePreprocessor.preprocess(self, nbman, *args, **kwargs)
+        return super(SoSPaperMillPreprocessor,
+                     self).preprocess(nbman, *args, **kwargs)
 
 class SoSExecutorEngine(Engine):
     """
