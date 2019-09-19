4b993a52507b3dd317c0f7e37a93b286fe1ac42f
Bo Peng <ben.bog@gmail.com>
2019-09-18 20:52:45 -0500

    Test render magics

--- test/test_engine.py
+++ test/test_engine.py
@@ -18,14 +18,33 @@ def load_notebook(notebook_name):
     nb.metadata.papermill['input_path'] = notebook_name
     return nb
 
-def get_output(cell):
-    res = ''
-    for output in cell.outputs:
-        if output['output_type'] == 'stream':
-            res += output['text']
-    return res
-
 class TestSoSExecutorEngine(unittest.TestCase):
+
+    def assertOutput(self, text, cell):
+        res = ''
+        for output in cell.outputs:
+            if output['output_type'] == 'stream':
+                res += output['text']
+            elif output['output_type'] == 'execute_result':
+                if 'text/plain' in output['data']:
+                    res += output['data']['text/plain']
+            elif output['output_type'] == 'display_data':
+                if 'text/plain' in output['data']:
+                    res += output['data']['text/plain']
+                if 'text/markdown' in output['data']:
+                    res += output['data']['text/markdown']
+        self.assertTrue(text in res, f'"{text}" not seem in output, "{res}" obtained')
+
+    def assertOutputType(self, output_type, cell):
+        resTypes = []
+        for output in cell.outputs:
+            print(output)
+            if output['output_type'] == 'stream':
+                resTypes.append(output['name'])
+            elif output['output_type'] in ('execute_result', 'display_data'):
+                resTypes.extend(list(output['data'].keys()))
+        self.assertTrue(output_type in resTypes, f'Output of type "{output_type}" not seem in output, "{resTypes}" obtained')
+
     def test_sos_executor_engine_execute(self):
         src_nb = load_notebook('sos_python3.ipynb')
         nb = SoSExecutorEngine.execute_notebook(
@@ -42,4 +61,15 @@ class TestSoSExecutorEngine(unittest.TestCase):
             self.assertEqual(
                 cell.metadata.papermill['status'], NotebookExecutionManager.COMPLETED
             )
-        self.assertTrue('101' in get_output(nb.cells[2]))
+        self.assertOutput('101', nb.cells[2])
+
+    def test_capture_expand_render(self):
+        src_nb = load_notebook('capture_expand_render.ipynb')
+        nb = SoSExecutorEngine.execute_notebook(
+            src_nb, 'sos', output_path='foo.ipynb', progress_bar=False, log_output=False
+        )
+        self.assertOutput('10', nb.cells[1])
+        self.assertOutput('hello', nb.cells[2])
+        self.assertOutput('hello', nb.cells[3])
+        self.assertOutput('header', nb.cells[4])
+        self.assertOutputType('text/markdown', nb.cells[4])
